115fbc671bdbabc61bee4ddc8887eb75d3505623
ForMula <trodion357@gmail.com>
2026-04-20 21:23:54 +0700

    Add files via upload

---
+++ import secrets.py
@@ -0,0 +1,16 @@
+import string
+import random
+def generate_password(length: int):
+    
+    if  length < 5:
+        length = 5
+    elif length > 20:
+        length = 20
+
+    characters = string.ascii_letters + string.digits + string.punctuation
+
+    password ='' .join(random.choice(characters)for _ in range(length))
+    return password
+                       
+
+print(generate_password(10))
